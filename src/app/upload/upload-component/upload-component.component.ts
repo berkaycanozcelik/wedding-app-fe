538387c7b5c13fa9b2a3f29ef6ab92d1792eb6c2
berkaycanozcelik <brkycn10@gmail.com>
2024-08-06 12:36:39 +0200

    updated upload url

--- src/app/upload/upload-component/upload-component.component.ts
+++ src/app/upload/upload-component/upload-component.component.ts
@@ -14,7 +14,7 @@ export class UploadComponent {
   selectedFiles: File[] = [];
   message = '';
   progress = 0;
-  private uploadUrl = 'http://127.0.0.1:8000/upload-file/'; // Updated to a generic file upload endpoint
+  private uploadUrl = '3.127.69.141/upload-file/'; // Updated to a generic file upload endpoint
 
   constructor(private http: HttpClient) {}
 
